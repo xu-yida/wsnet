216e5d070a3838947cd282cefc53f58d87eacfb7
Adam <xuyida@ict.acn.cn>
2018-04-25 11:03:08 +0800

    add log file

--- include/options.h
+++ include/options.h
@@ -44,7 +44,7 @@
 #define LOG_WORLDSENS
 
 // <-RF00000000-AdamXu-2018/04/25-write log in file
-#define ADAM_LOG_FILE
+//#define ADAM_LOG_FILE
 #define ADAM_LOG_NAME "adam_log.log"
 // ->RF00000000-AdamXu
 
