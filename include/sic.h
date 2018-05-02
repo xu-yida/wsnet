3142561ccd7108db2e1fc1dea9ff386ef0f8fb19
Adam <xuyida@ict.acn.cn>
2018-05-02 10:23:14 +0800

    add SIC module

---
+++ include/sic.h
@@ -0,0 +1,14 @@
+/**
+ *  \file   sic.h
+ *  \brief  Sic Definitions
+ *  \author Adam Xu
+ *  \date   2018
+ **/
+#ifndef __sic_public__
+#define __sic_public__
+
+#define MIN_SIC_REMAIN 0
+
+#define MIN_SIC_ITERATION 2
+
+#endif //__radio_public__
