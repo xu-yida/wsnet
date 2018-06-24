8cb485b8e5508b134a3b0def823d4ae17ac827fc
Adam <xuyida@ict.acn.cn>
2018-06-24 20:05:53 +0800

    add log for channel check

--- src/radio.c
+++ src/radio.c
@@ -11,6 +11,9 @@
 /* ************************************************** */
 double radio_get_noise(call_t *c) {
     entity_t *entity = get_entity_by_id(c->entity);
+	// <-RF00000000-AdamXu-2018/06/24-add log for sic channel check.
+	PRINT_RADIO("B: entity->model->type=%d", entity->model->type);
+	// ->RF00000000-AdamXu
     if (entity->model->type != MODELTYPE_RADIO)
         return MIN_DBM;
     return entity->methods->radio.get_noise(c);    
@@ -18,6 +21,9 @@ double radio_get_noise(call_t *c) {
 
 double radio_get_cs(call_t *c) {
     entity_t *entity = get_entity_by_id(c->entity);
+	// <-RF00000000-AdamXu-2018/06/24-add log for sic channel check.
+	PRINT_RADIO("B: entity->model->type=%d", entity->model->type);
+	// ->RF00000000-AdamXu
     if (entity->model->type != MODELTYPE_RADIO)
         return MIN_DBM;
     return entity->methods->radio.get_cs(c);    
