34c3a65fcdc0dba1f07934f296fc82abe94b7c99
Adam <xuyida@ict.acn.cn>
2018-07-06 11:09:54 +0800

    add packet time log

--- models/application/cbr.c
+++ models/application/cbr.c
@@ -180,7 +180,7 @@ void tx(call_t *c) {
 // <-RF00000000-AdamXu-2018/07/06-test sic.
 #ifdef ADAM_TEST
 	PRINT_RADIO("application B: packet->id=%d, c->node=%d\n", packet->id, c->node);
-	PRINT_ROUTING("get_time()=%"PRId64"\n", get_time();
+	PRINT_ROUTING("get_time()=%"PRId64"\n", get_time());
 #endif
 // ->RF00000000-AdamXu
     if (SET_HEADER(&c0, packet, &destination) == -1) {
