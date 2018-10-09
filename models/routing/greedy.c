a2777df6ffd771d586a9af29311ca2d6cac3920e
Adam <xuyida@ict.acn.cn>
2018-10-10 00:13:56 +0800

    add energy cinsumation result

--- models/routing/greedy.c
+++ models/routing/greedy.c
@@ -472,6 +472,7 @@ void rx(call_t *c, packet_t *packet) {
 		PRINT_RESULT("%d priority0 packets received, %d priority1 packets received\n", g_num_r_priority0, g_num_r_priority1);
 		PRINT_RESULT("average delay is %f\n", g_delay/g_num_r);
 		PRINT_RESULT("average delay of priority0 packets is %f, average delay of priority1 packets is %f\n", g_delay_priority0/g_num_r_priority0, g_delay_priority1/g_num_r_priority1);
+		PRINT_RESULT("node %d consumes energy: %f\n", c->node, battery_consumed(c));
 #endif//ADAM_TEST
 // ->RF00000000-AdamXu
         while (i--) {
