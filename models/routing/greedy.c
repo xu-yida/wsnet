de994587edade2a085be22667af900c4f9cf1fea
Adam <xuyida@ict.acn.cn>
2018-10-17 23:29:41 +0800

    add max delay

--- models/routing/greedy.c
+++ models/routing/greedy.c
@@ -70,7 +70,8 @@ struct nodedata {
 double g_delay = 0;
 double g_delay_priority0 = 0;
 double g_delay_priority1 = 0;
-double g_delay_max = 0;
+uint64_t g_delay_max0 = 0;
+uint64_t g_delay_max1 = 0;
 int g_num_r = 0;
 int g_num_r_priority0 = 0;
 int g_num_r_priority1 = 0;
@@ -459,26 +460,31 @@ void rx(call_t *c, packet_t *packet) {
 // <-RF00000000-AdamXu-2018/07/06-test sic.
 #ifdef ADAM_TEST
 		delay = get_time() - header->time_start;
-		if(delay > g_delay_max)
-		{
-			g_delay_max = delay;
-		}
 		g_delay += delay;
 		if(0 == packet->type)
 		{
+			if(delay > g_delay_max0)
+			{
+				g_delay_max0 = delay;
+			}
 			g_delay_priority0 += delay;
 			g_num_r_priority0++;
 		}
 		else if(1 == packet->type)
 		{
+			if(delay > g_delay_max1)
+			{
+				g_delay_max1 = delay;
+			}
 			g_delay_priority1 += delay;
 			g_num_r_priority1++;
 		}
 		g_num_r++;
-		PRINT_RESULT("node %d received packet, delay=%"PRId64"\n", c->node, delay);
+		//PRINT_RESULT("node %d received packet, delay=%"PRId64"\n", c->node, delay);
 		PRINT_RESULT("%d packets received\n", g_num_r);
 		PRINT_RESULT("%d priority0 packets received, %d priority1 packets received\n", g_num_r_priority0, g_num_r_priority1);
-		PRINT_RESULT("average delay is %f, maximal delay is %f\n", g_delay/g_num_r, g_delay_max);
+		PRINT_RESULT("average delay is %f\n", g_delay/g_num_r);
+		PRINT_RESULT("maximal priority0 delay is %f, maximal priority1 delay is %f\n", g_delay_max0, g_delay_max1);
 		PRINT_RESULT("average delay of priority0 packets is %f, average delay of priority1 packets is %f\n", g_delay_priority0/g_num_r_priority0, g_delay_priority1/g_num_r_priority1);
 #endif//ADAM_TEST
 // ->RF00000000-AdamXu
